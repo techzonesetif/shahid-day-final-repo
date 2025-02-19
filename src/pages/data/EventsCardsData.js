e7f0baad85179e7300df418d4a6718ba98588519
abdurrahmen <abderrahmendekoune@gmail.com>
2025-02-19 03:40:35 +0100

    Events Link edited

--- src/pages/data/EventsCardsData.js
+++ src/pages/data/EventsCardsData.js
@@ -11,41 +11,41 @@ export const cardsData = {
       description: "Marked the start of Algeria's war for independence from France, led by the FLN with coordinated attacks across the country",
       img: outbreakImg,
       date: "November 1, 1954",
-      link: "/article/1"
+      link: "/articles/The_Outbreak_of_the_Revolution"
     },
     card2: {
       title: "Soummam Conference",
       description: "A crucial meeting that structured the FLN's political leadership and military strategy to strengthen the independence movement",
       img: soummamImg,
       date: "August 20, 1956",
-      link: "/article/2"
+      link: "/articles/The_Soummam_Conference"
     },
     card3: {
       title: "Battle of Algiers",
       description: " Intense urban warfare between the FLN and French forces in Algiers, known for guerrilla tactics and harsh counter-insurgency",
       img: algiersImg,
       date: "January - October, 1957",
-      link: "/article/3"
+      link: "/articles/_The_Battle_of_Algiers_1957"
     },
     card4: {
       title: "Reggane Nuclear Tests",
       description: " France conducted its first nuclear test in the Algerian Sahara, showcasing its nuclear capability during the colonial period",
       img: regganImg,
       date: "February 13, 1960",
-      link: "/article/4"
+      link: "/articles/Reggane_Nuclear_Tests"
     },
     card5: {
       title: "Bombing of Sakiet Sidi Youssef",
       description: "French forces bombed a Tunisian village, targeting FLN fighters but also killing many civilians, straining French-Tunisian relations",
       img: sidiYoucefImg,
       date: "February 8, 1958",
-      link: "/article/5"
+      link: "/articles/Reggane_Nuclear_Tests"
     },
     card6: {
       title: "Paris Massacre",
       description: "Peaceful Algerian protesters were brutally suppressed by French police in Paris, leading to numerous deaths and disappearances",
       img: parisMassacreImg,
       date: "October 17, 1961",
-      link: "/article/6"
+      link: "/articles/Paris_Massacre"
     },
 }
